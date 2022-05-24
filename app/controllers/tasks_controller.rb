6bd2c0a4577786a7334728c16c3ea3776ebfa4e0
Adriana Osorio <driosorio@gmail.com>
2022-05-24 16:47:10 -0300

    index, show, new tsk and edit task completed, no stylr yet

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,7 +3,33 @@ class TasksController < ApplicationController
     @tasks = Task.all
   end
 
+  def new
+    @task = Task.new # needed to instantiate the form_for
+  end
+
   def show
     @task = Task.find(params[:id])
   end
+
+  def create
+    @task = Task.new(task_params)
+    @task.save
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(task_params)
+    # Will raise ActiveModel::ForbiddenAttributesError
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
+  end
+
 end
